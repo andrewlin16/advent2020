5a86c25b1463d196486b72fa2adacf4c97a86980
Andrew Lin <andrewlin16@gmail.com>
2020-12-12 17:51:39 -0600

    Add debug.sh script
    debug.sh compiles a temp binary with debug symbols, launches gdb, and
    then cleans up the temp binary.

---
+++ debug.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+
+if [[ $# -lt 1 ]] ; then
+	echo "Usage: $0 <name>"
+	exit 1
+fi
+
+srcfile="src/$1.cpp"
+binfile="$(mktemp)"
+
+grep -q "#include <util.h>" "${srcfile}" && utilargs="-Isrc src/util.cpp" || utilargs=""
+
+g++ -std=c++20 -g ${utilargs} "${srcfile}" -o "${binfile}"
+gdb "${binfile}"
+rm "${binfile}"
