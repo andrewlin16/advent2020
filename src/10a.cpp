89dd7accf7e56ae1e2307ee4097b07da3a3b494e
Andrew Lin <andrewlin16@gmail.com>
2020-12-16 21:18:14 -0600

    Add solution to 10a

---
+++ src/10a.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iterator>
+#include <set>
+
+int main() {
+	std::istream_iterator<int> input_begin(std::cin), input_end;
+	const std::set<int> adapters(input_begin, input_end);
+
+	int joltage = 0;
+	int count_1 = 0;
+	int count_3 = 1;
+
+	for (auto iter = adapters.begin(); iter != adapters.end(); ++iter) {
+		const int value = *iter;
+		const int diff = value - joltage;
+
+		if (diff == 1) {
+			count_1++;
+		} else if (diff == 3) {
+			count_3++;
+		}
+
+		joltage = value;
+	}
+
+	std::cout << (count_1 * count_3) << std::endl;
+	return 0;
+}
