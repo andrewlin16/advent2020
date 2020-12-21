b3bfb664f2c248e63f58595cdf6fae2353edeea1
Andrew Lin <andrewlin16@gmail.com>
2020-12-21 02:31:35 -0600

    Add ParseVectorString function to util

--- src/util.h
+++ src/util.h
@@ -8,9 +8,24 @@
 namespace util {
 
 const std::vector<std::string> ReadInput();
+
 const std::vector<std::vector<std::string>> ReadInputGroups();
+
 void Tokenize(const std::string&, const char,
 	const std::function<void(const std::string&, int)>&);
+
+template<typename T>
+std::vector<T> ParseVectorString(
+		const std::string& str, const char token,
+		const std::function<T(const std::string&)>& parse_func) {
+	std::vector<T> result;
+
+	Tokenize(str, token, [&result, &parse_func](const std::string& str, int i) {
+		result.push_back(parse_func(str));
+	});
+
+	return result;
+}
 
 }
 
