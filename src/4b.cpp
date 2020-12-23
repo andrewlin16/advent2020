371c14f4a0051f2f79a17380ec0f548f505add47
Andrew Lin <andrewlin16@gmail.com>
2020-12-22 19:01:50 -0600

    More style nitting (indents) in 4b

--- src/4b.cpp
+++ src/4b.cpp
@@ -24,58 +24,57 @@ const std::unordered_set<std::string> kValidEyeColors = {
 	"amb", "blu", "brn", "gry", "grn", "hzl", "oth"
 };
 
-const std::unordered_map<std::string, Validator>
-	kValidators = {
-		{"byr", year_validator(1920, 2020)},
-		{"iyr", year_validator(2010, 2020)},
-		{"eyr", year_validator(2020, 2030)},
-		{"hgt", [](const std::string& s) {
-			if (s.size() <= 2) {
-				return false;
-			}
+const std::unordered_map<std::string, Validator> kValidators = {
+	{"byr", year_validator(1920, 2020)},
+	{"iyr", year_validator(2010, 2020)},
+	{"eyr", year_validator(2020, 2030)},
+	{"hgt", [](const std::string& s) {
+		if (s.size() <= 2) {
+			return false;
+		}
 
-			const int value = std::stoi(s.substr(0, s.size() - 2));
-			const std::string unit = s.substr(s.size() - 2, 2);
+		const int value = std::stoi(s.substr(0, s.size() - 2));
+		const std::string unit = s.substr(s.size() - 2, 2);
 
-			if (unit == "cm") {
-				return value >= 150 && value <= 193;
-			} else if (unit == "in") {
-				return value >= 59 && value <= 76;
-			} else {
-				return false;
-			}
-		}},
-		{"hcl", [](const std::string& s) {
-			if (s.size() != 7) {
-				return false;
-			}
+		if (unit == "cm") {
+			return value >= 150 && value <= 193;
+		} else if (unit == "in") {
+			return value >= 59 && value <= 76;
+		} else {
+			return false;
+		}
+	}},
+	{"hcl", [](const std::string& s) {
+		if (s.size() != 7) {
+			return false;
+		}
 
-			if (s[0] != '#') {
-				return false;
-			}
+		if (s[0] != '#') {
+			return false;
+		}
 
-			return std::all_of(s.begin() + 1, s.end(), [](char c) {
-				return std::isxdigit(c) && !std::isupper(c);
-			});
-		}},
-		// alternatively:
-		//   std::bind(
-		//     &decltype(kValidEyeColors)::contains, &kValidEyeColors,
-		//     std::placeholders::_1)
-		// but like, why?
-		{"ecl", [](const std::string& s) {
-			return kValidEyeColors.contains(s);
-		}},
-		{"pid", [](const std::string& s) {
-			if (s.size() != 9) {
-				return false;
-			}
+		return std::all_of(s.begin() + 1, s.end(), [](char c) {
+			return std::isxdigit(c) && !std::isupper(c);
+		});
+	}},
+	// alternatively:
+	//   std::bind(
+	//     &decltype(kValidEyeColors)::contains, &kValidEyeColors,
+	//     std::placeholders::_1)
+	// but like, why?
+	{"ecl", [](const std::string& s) {
+		return kValidEyeColors.contains(s);
+	}},
+	{"pid", [](const std::string& s) {
+		if (s.size() != 9) {
+			return false;
+		}
 
-			return std::all_of(s.begin(), s.end(), [](char c) {
-				return std::isdigit(c);
-			});
-		}},
-	};
+		return std::all_of(s.begin(), s.end(), [](char c) {
+			return std::isdigit(c);
+		});
+	}},
+};
 
 int main() {
 	const std::vector<std::vector<std::string>> input = util::ReadInputGroups();
