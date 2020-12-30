a45e599fdfd2276f4f5deaaccb374a639fdd890c
Andrew Lin <andrewlin16@gmail.com>
2020-12-30 02:07:05 -0600

    Add solution to 22b

---
+++ src/22b.cpp
@@ -0,0 +1,82 @@
+#include <algorithm>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <string>
+#include <vector>
+
+#include <util.h>
+
+using Deck = std::deque<int>;
+using History = std::set<std::pair<Deck, Deck>>;
+
+int stoi(const std::string& a) {
+	return std::stoi(a);
+}
+
+bool PlayGame(Deck&, Deck&);
+
+bool P1Won(const Deck& deck1, const Deck& deck2) {
+	const size_t top_deck1 = deck1.front();
+	const size_t top_deck2 = deck2.front();
+
+	if (top_deck1 > deck1.size() - 1 || top_deck2 > deck2.size() - 1) {
+		return top_deck1 > top_deck2;
+	}
+
+	Deck new_deck1(deck1.begin() + 1, deck1.begin() + 1 + top_deck1);
+	Deck new_deck2(deck2.begin() + 1, deck2.begin() + 1 + top_deck2);
+	return PlayGame(new_deck1, new_deck2);
+}
+
+bool PlayGame(Deck& deck1, Deck& deck2) {
+	History history;
+
+	while (!deck1.empty() && !deck2.empty()) {
+		if (history.contains({deck1, deck2})) {
+			return true;
+		}
+
+		const int top_deck1 = deck1.front();
+		const int top_deck2 = deck2.front();
+		const bool p1_winner = P1Won(deck1, deck2);
+
+		history.emplace(deck1, deck2);
+		deck1.pop_front();
+		deck2.pop_front();
+
+		if (p1_winner) {
+			deck1.insert(deck1.end(), {top_deck1, top_deck2});
+		} else {
+			deck2.insert(deck2.end(), {top_deck2, top_deck1});
+		}
+	}
+
+	return deck2.empty();
+}
+
+int main() {
+	const std::vector<std::vector<std::string>> input = util::ReadInputGroups();
+
+	Deck deck1;
+	Deck deck2;
+
+	std::transform(input[0].begin() + 1, input[0].end(),
+		std::back_inserter(deck1), stoi);
+	std::transform(input[1].begin() + 1, input[1].end(),
+		std::back_inserter(deck2), stoi);
+
+	const bool p1_winner = PlayGame(deck1, deck2);
+	const Deck& winning_deck = p1_winner ? deck1 : deck2;
+	const size_t size = winning_deck.size();
+
+	int result = 0;
+	for (size_t i = 0; i < size; ++i) {
+		result += (size - i) * winning_deck[i];
+	}
+
+	std::cout << result << std::endl;
+	return 0;
+}
