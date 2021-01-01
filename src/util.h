7897bfc46e3c723367b9de688d4283bba5d28da4
Andrew Lin <andrewlin16@gmail.com>
2021-01-01 16:11:00 -0600

    Make HashCombine variadic, add TupleHash
    HashCombine can accept a variable number of arguments, implemented via
    recursive template functions. With HashCombine being variadic, this
    allows for a "simple" TupleHash implementation via pack expansion and
    apply (depending on your definition of "simple").
    
    Think this might be approaching the realm of abusing C++ language
    features/magic, but...at least it works?

--- src/util.h
+++ src/util.h
@@ -3,6 +3,7 @@
 
 #include <functional>
 #include <string>
+#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -17,14 +18,29 @@ std::vector<int> ParseIntList(const std::string&);
 // Based on https://stackoverflow.com/a/27216842, which seems to be based on
 // Boost libraries.
 template<typename T>
-size_t HashCombine(const size_t seed, const T value) {
-	return seed ^ (value + 0x9e3779b9 + (seed << 6) + (seed >> 2));
+size_t HashCombine(const size_t seed, const T& value) {
+	return seed ^ (
+		std::hash<T>{}(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2));
+}
+
+template<typename T, typename... Targs>
+size_t HashCombine(const size_t seed, const T& value, const Targs... args) {
+	return HashCombine(HashCombine(seed, value), args...);
 }
 
 template<typename T1, typename T2>
 struct PairHash {
 	size_t operator()(const std::pair<T1, T2>& v) const {
-		return HashCombine(std::hash<T1>{}(v.first), std::hash<T2>{}(v.second));
+		return HashCombine(std::hash<T1>{}(v.first), v.second);
+	}
+};
+
+template<typename... Ts>
+struct TupleHash {
+	size_t operator()(const std::tuple<Ts...>& v) const {
+		return std::apply([](const Ts&... args) {
+			return HashCombine(sizeof...(Ts), args...);
+		}, v);
 	}
 };
 
